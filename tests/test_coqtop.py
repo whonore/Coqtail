95f77cae05538c1652d9248f1df12ec226b8bae7
whonore <wolfhonore@gmail.com>
2018-02-17 18:38:55 -0500

    more tests

--- tests/test_coqtop.py
+++ tests/test_coqtop.py
@@ -61,12 +61,52 @@ def test_rewind_start(coq):
 def test_dispatch_rewind(coq):
     """Rewinding should cancel out in_script dispatches."""
     old_state = get_state(coq)
-    coq.dispatch('Let x := 1.', timeout=TIMEOUT)
+    succ, _, _ = coq.dispatch('Let x := 1.', timeout=TIMEOUT)
+    assert succ
     coq.rewind(1)
     assert old_state == get_state(coq)
-    coq.dispatch('Print nat.', timeout=TIMEOUT)
+    succ, _, _ = coq.dispatch('Print nat.', timeout=TIMEOUT)
+    assert succ
     coq.rewind(1)
     assert old_state == get_state(coq)
-    coq.dispatch('Test Silent.', timeout=TIMEOUT)
+    succ, _, _ = coq.dispatch('Test Silent.', timeout=TIMEOUT)
+    assert succ
     coq.rewind(1)
+    assert old_state == get_state(coq)
+
+
+def test_dispatch_not_in_script(coq):
+    """Dispatch with not in_script arguments shouldn't change the state."""
+    old_state = get_state(coq)
+    coq.dispatch('Print nat.', in_script=False, timeout=TIMEOUT)
+    assert old_state == get_state(coq)
+    coq.dispatch('Test Silent.', in_script=False, timeout=TIMEOUT)
+    assert old_state == get_state(coq)
+
+
+def test_goals_no_change(coq):
+    """Calling goals will not change the state."""
+    old_state = get_state(coq)
+    coq.goals(timeout=TIMEOUT)
+    assert old_state == get_state(coq)
+
+
+def test_mk_cases_no_change(coq):
+    """Calling mk_cases will not change the state."""
+    old_state = get_state(coq)
+    coq.mk_cases('nat', timeout=TIMEOUT)
+    assert old_state == get_state(coq)
+
+
+def test_advance_fail(coq):
+    """If advance fails then the state will not change."""
+    old_state = get_state(coq)
+    fail, _, _ = coq.advance('SyntaxError', timeout=TIMEOUT)
+    assert not fail
+    assert old_state == get_state(coq)
+    succ, _, _ = coq.advance('Lemma x : False.', timeout=TIMEOUT)
+    assert succ
+    old_state = get_state(coq)
+    fail, _, _ = coq.advance('reflexivity.', timeout=TIMEOUT)
+    assert not fail
     assert old_state == get_state(coq)
