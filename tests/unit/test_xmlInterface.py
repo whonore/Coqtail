5caae50bc757bfd205698b8a6ecb64e46fed3731
whonore <wolfhonore@gmail.com>
2020-08-22 21:24:24 -0400

    Update test header comments

--- tests/unit/test_xmlInterface.py
+++ tests/unit/test_xmlInterface.py
@@ -1,10 +1,6 @@
 # -*- coding: utf8 -*-
-"""
-File: test_xmlInterface.py
-Author: Wolf Honore
-
-Description: Unit tests for xmlInterface.py.
-"""
+# Author: Wolf Honore
+"""XMLInterface marshalling unit tests."""
 
 from __future__ import absolute_import
 from __future__ import division
