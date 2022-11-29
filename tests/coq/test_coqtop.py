b4f0bd51aa62ae5a8de26d3f36e61d646f9d47f9
whonore <wolfhonore@gmail.com>
2022-11-29 11:02:37 -0500

    Skip test for 8.4

--- tests/coq/test_coqtop.py
+++ tests/coq/test_coqtop.py
@@ -289,6 +289,8 @@ def test_start_noinit() -> None:
     """-noinit does not cause Coqtail to hang."""
     ct = Coqtop()
     res, _ = ct.start(None, None, "", ["-noinit"])
+    if ct.xml.version < (8, 5, 0):
+        pytest.skip("Only 8.5+ supports -noinit")
     assert isinstance(res, dict)
     assert ct.xml is not None
     succ, _, _, _ = ct.dispatch("Set Implicit Arguments.")
