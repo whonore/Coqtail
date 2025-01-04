851130c00efcd00d96fd7d2afb8bafb0888e7c9d
Wolf Honore <wolfhonore@gmail.com>
2025-01-04 15:17:09 -0800

    Fix CI (#376)
    * Appease mypy and pylint
    
    * Fix nixpkgs url for 8.5 and 8.6

--- ci/coq.nix
+++ ci/coq.nix
@@ -3,13 +3,18 @@ with builtins;
 
 assert version != null || tox_version != null;
 let
-  url_8_4 = "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz";
+  pkg_urls = {
+    "8.4" = "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz";
+    "8.5" = "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz";
+    "8.6" = "https://github.com/NixOS/nixpkgs/archive/24.05.tar.gz";
+  };
   dot_version = if version != null then
     version
   else
     concatStringsSep "."
     (filter (s: s != "") (match "coq([0-9]|master)([0-9]*)-.*" tox_version));
-in if dot_version == "8.4" then
-  (import (fetchTarball url_8_4) { }).coq_8_4
+  pkg_name = "coq_" + replaceStrings [ "." ] [ "_" ] dot_version;
+in if hasAttr dot_version pkg_urls then
+  (import (fetchTarball pkg_urls.${dot_version}) { }).${pkg_name}
 else
   pkgs.coq.override ({ version = dot_version; })
